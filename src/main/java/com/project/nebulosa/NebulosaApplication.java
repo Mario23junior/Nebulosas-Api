609c09a93d5d0190c5676074dd6e40e937bc9147
Mario23junior <mariojunior3251@gmail.com>
2021-05-25 05:35:38 -0300

    Iniciando api de exibiçao de nebulosas

---
+++ src/main/java/com/project/nebulosa/NebulosaApplication.java
@@ -0,0 +1,13 @@
+package com.project.nebulosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NebulosaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NebulosaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/nebulosa/NebulosaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.nebulosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NebulosaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
