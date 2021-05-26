787cbfd366ee40eb3b9e96fbfb1f9f4b68343bd4
Mario23junior <mariojunior3251@gmail.com>
2021-05-26 09:50:59 -0300

    Criando repository do modelo de dados de Nebulosa

---
+++ src/main/java/com/project/nebulosa/Repositorys/NebulosaRepository.java
@@ -0,0 +1,9 @@
+package com.project.nebulosa.Repositorys;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.nebulosa.model.Nebulosa;
+
+public interface NebulosaRepository extends JpaRepository<Nebulosa, Long>{
+
+}
