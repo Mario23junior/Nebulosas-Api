9a5c82a2a07d863acffea926a158e9602c3b04a7
Mario23junior <mariojunior3251@gmail.com>
2021-05-26 09:59:01 -0300

    Criando repository da base BaseRelato

---
+++ src/main/java/com/project/nebulosa/Repositorys/RepositoryBaseRelato.java
@@ -0,0 +1,9 @@
+package com.project.nebulosa.Repositorys;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.nebulosa.model.BaseRelato;
+
+public interface RepositoryBaseRelato extends JpaRepository<BaseRelato, Long>{
+
+}
