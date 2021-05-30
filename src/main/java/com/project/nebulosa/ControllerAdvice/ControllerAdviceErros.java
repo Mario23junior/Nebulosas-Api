385756df59adebaeadfa09679b936c1a0e79ea2f
Mario23junior <mariojunior3251@gmail.com>
2021-05-30 07:06:23 -0300

    definindo padrão de requisisao de erros de retorno

--- src/main/java/com/project/nebulosa/ControllerAdvice/ControllerAdviceErros.java
+++ src/main/java/com/project/nebulosa/ControllerAdvice/ControllerAdviceErros.java
@@ -1,35 +1,40 @@
 package com.project.nebulosa.ControllerAdvice;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Date;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.google.common.net.HttpHeaders;
+import com.project.nebulosa.Exception.ResourceNotFoundException;
 
 @RestControllerAdvice
-public class ControllerAdviceErros extends ResponseEntityExceptionHandler{
+public class ControllerAdviceErros{
 	
-	   protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-	        List<ErrorObject> errors = getErrors(ex);
-	        ApiErros errorResponse = getErrorResponse(ex, status, errors);
-	        return new ResponseEntity<>(errorResponse, status);
-	    }
-
-	    private ApiErros getErrorResponse(MethodArgumentNotValidException ex, HttpStatus status, List<ErrorObject> errors) {
-	        return new ApiErros("Requisição possui campos inválidos", status.value(),
-	                status.getReasonPhrase(), ex.getBindingResult().getObjectName(), errors);
-	    }
-
-	    private List<ErrorObject> getErrors(MethodArgumentNotValidException ex) {
-	        return ex.getBindingResult().getFieldErrors().stream()
-	                .map(error -> new ErrorObject(error.getDefaultMessage(), error.getField(), error.getRejectedValue()))
-	                .collect(Collectors.toList());
-	    }
+	 @ExceptionHandler(ResourceNotFoundException.class)
+	  @ResponseStatus(value = HttpStatus.NOT_FOUND)
+	  public ApiErros resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+	    ApiErros message = new ApiErros(
+	        HttpStatus.NOT_FOUND.value(),
+	        new Date(),
+	        ex.getMessage(),
+	        request.getDescription(false));
+	    
+	    return message;
+	  }
+	  
+	  @ExceptionHandler(Exception.class)
+	  @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+	  public ApiErros globalExceptionHandler(Exception ex, WebRequest request) {
+	    ApiErros message = new ApiErros(
+	        HttpStatus.INTERNAL_SERVER_ERROR.value(),
+	        new Date(),
+	        ex.getMessage(),
+	        request.getDescription(false));
+	    
+	    return message;
+	  }
 }
 
