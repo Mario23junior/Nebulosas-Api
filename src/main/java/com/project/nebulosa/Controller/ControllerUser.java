da87100eecff98fe5726239abdf653e2a7ae281b
Mario23junior <mariojunior3251@gmail.com>
2021-05-31 04:50:42 -0300

    Criando controller e Service da base de User

---
+++ src/main/java/com/project/nebulosa/Controller/ControllerUser.java
@@ -0,0 +1,5 @@
+package com.project.nebulosa.Controller;
+
+public class ControllerUser {
+
+}
---
+++ src/main/java/com/project/nebulosa/Controller/Service/ServiceUser.java
@@ -0,0 +1,9 @@
+package com.project.nebulosa.Controller.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceUser {
+   
+ 
+}
