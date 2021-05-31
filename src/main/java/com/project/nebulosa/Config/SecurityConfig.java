2f6c4893d7933371182ad6e8d61835ddefd5dbfd
Mario23junior <mariojunior3251@gmail.com>
2021-05-31 06:47:44 -0300

    Refatorando api para version anterior e restruturando

--- src/main/java/com/project/nebulosa/Config/SecurityConfig.java
+++
@@ -1,9 +0,0 @@
-package com.project.nebulosa.Config;
-
-public class SecurityConfig {
-    
-	public final String SIGN_UP_URL = "/user/record";
-	public final String KEY = "q3t6w9z$C&F)J@NcQfTjWnZr4u7x!A%D*G-KaPdSgUkXp2s5v8y/B?E(H+MbQeTh";
-	public final String HEADER_NAME = "Authorization";
-	public final Long EXPIRATION_TIME = 1000L*60*30;
-}
