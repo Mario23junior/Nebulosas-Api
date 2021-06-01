e0ea40f13a1ab2ebf2d83b92944366c4bf84d95e
Mario23junior <mariojunior3251@gmail.com>
2021-06-01 03:35:34 -0300

    Criando bean para criptografar e deciptografar

--- src/main/java/com/project/nebulosa/Config/SecurityConfig.java
+++ src/main/java/com/project/nebulosa/Config/SecurityConfig.java
@@ -1,17 +1,25 @@
 package com.project.nebulosa.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
    
+ 	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
- 		super.configure(auth);
-	}
+		
+ 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
