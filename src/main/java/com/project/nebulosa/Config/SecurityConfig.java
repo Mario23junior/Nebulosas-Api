2d443b8163e330130b066c267ec4f3ad145b3329
Mario23junior <mariojunior3251@gmail.com>
2021-06-01 05:49:33 -0300

    Criando camada de service security para consultar dados cadastrados no banco de dados para efetuar login na API

--- src/main/java/com/project/nebulosa/Config/SecurityConfig.java
+++ src/main/java/com/project/nebulosa/Config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.project.nebulosa.Config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,9 +9,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.project.nebulosa.Service.ServiceBase.SecurityService;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
    
+	@Autowired
+	private SecurityService securityService;
+
  	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -18,11 +24,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		             .passwordEncoder(passwordEncoder())
-		             .withUser("exemplo")
-		             .password(passwordEncoder().encode("123"))
-		             .roles("USER","ADMIN");
+		auth.
+		   userDetailsService(securityService)
+		   .passwordEncoder(passwordEncoder());
  	}
 	
 	@Override
