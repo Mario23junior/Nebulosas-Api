429816535df5563754c7914864d383dc0a33094a
Mario23junior <mariojunior3251@gmail.com>
2021-06-02 07:07:24 -0300

    emplementando logica de verificacao de login de usuarios cadastrados

--- src/main/java/com/project/nebulosa/Repositorys/RepositoryUsuario.java
+++ src/main/java/com/project/nebulosa/Repositorys/RepositoryUsuario.java
@@ -1,9 +1,12 @@
 package com.project.nebulosa.Repositorys;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.nebulosa.model.Usuario;
 
 public interface RepositoryUsuario extends JpaRepository<Usuario, Long> {
-
+  
+	Optional<Usuario> findByLogin(String login);
 }
